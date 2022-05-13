1660fe54fb0f783e536d1e7edb2303ceb5a8599c
Matama091 <matama@pluslab.org>
2022-05-13 09:52:16 +0900

    add hash function

--- multithread/go.mod
+++ multithread/go.mod
@@ -1,23 +1,3 @@
-module main
+module github.com/Matama091/play
 
-go 1.16
-
-require (
-	github.com/codahale/aesnicheck v0.0.0-20140702143623-349fcc471aac // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/go-sql-driver/mysql v1.6.0
-	github.com/google/uuid v1.3.0
-	github.com/jinzhu/gorm v1.9.16
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/joho/godotenv v1.3.0
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.7.0
-	go.uber.org/atomic v1.9.0 // indirect
-	go.uber.org/multierr v1.7.0 // indirect
-	go.uber.org/tools v0.0.0-20190618225709-2cfd321de3ee // indirect
-	go.uber.org/zap v1.18.1
-	golang.org/x/lint v0.0.0-20190930215403-16217165b5de // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
-	honnef.co/go/tools v0.0.1-2019.2.3 // indirect
-)
+go 1.17
--- multithread/main.go
+++ multithread/main.go
@@ -2,68 +2,87 @@ package main
 
 import (
 	"bufio"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"os"
+	"runtime"
 	"sync"
 )
 
 type data struct {
+	count int
 	sync.Mutex
 	text   string
-	buffer *[]byte
+	buffer []byte
 }
 
-func HASH(inc chan *data) {
+func HASH(inc chan *data, w *sync.WaitGroup) {
+	defer w.Done()
+
 	for elme := range inc {
-		if elme == nil {
-			return
-		}
-		fmt.Println("s")
+		checksum := sha256.Sum256([]byte(elme.text))
+		elme.buffer = checksum[:]
+
+		fmt.Println(elme.count)
+
 		elme.Unlock()
 	}
 }
 
-func Write(outc chan *data) {
+func Write(outc chan *data, w *sync.WaitGroup) {
+	defer w.Done()
+
 	for elme := range outc {
-		if elme == nil {
-			return
-		}
 		elme.Lock()
-		fmt.Println("s")
+
+		fmt.Println(elme.count, hex.Dump(elme.buffer))
+
 		elme.Unlock()
 	}
 }
 
 func main() {
 	var wait sync.WaitGroup
-	var inc chan *data
-	var outc chan *data
+	inc := make(chan *data)
+	outc := make(chan *data)
+
+	cpus := runtime.NumCPU()
+
+	wait.Add(cpus)
+	for i := 0; i < cpus; i++ {
+		go HASH(inc, &wait)
+	}
+
+	// wait.Add(1)
+	// go HASH(inc, &wait)
 
 	wait.Add(1)
-	go func() {
-		defer wait.Done()
-		HASH(inc)
-	}()
-
-	wait.Add(1)
-	go func() {
-		defer wait.Done()
-		Write(outc)
-	}()
+	go Write(outc, &wait)
 
 	f, _ := os.Open("text.txt")
 	defer f.Close()
 
 	sc := bufio.NewScanner(f)
 
+	i := 0
 	for sc.Scan() {
+		i++
+		// defer close(outc)
 		line := sc.Text()
-		data := data{}
-		data.text = line
+		data := data{
+			count:  i,
+			text:   line,
+			buffer: nil,
+			Mutex:  sync.Mutex{},
+		}
 		data.Lock()
 		inc <- &data
 		outc <- &data
 	}
+
+	close(inc)
+	close(outc)
 
 	wait.Wait()
 
